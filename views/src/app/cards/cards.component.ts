d0a1d654f3399250eecb50d61148ad43d55cadf8
Drageloz <dragelozr@gmail.com>
2020-11-22 12:00:08 -0500

    arreglo de la pagina y mejoras en visualización

---
+++ views/src/app/cards/cards.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.sass']
+})
+export class CardsComponent implements OnInit {
+
+  cards: any = [
+    {name: 'charmander', description: 'Charmander', size: '7 cm', qualification: 4.8, url: 'https://scontent.fbaq1-1.fna.fbcdn.net/v/t1.0-9/119548048_139407721194231_2019234050008204393_o.jpg?_nc_cat=103&ccb=2&_nc_sid=8bfeb9&_nc_ohc=w3ZFUCnZ6dMAX92p_My&_nc_ht=scontent.fbaq1-1.fna&oh=a325ed7ac7fc4da1974fa721c71cc457&oe=5FDF99BE'}
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
